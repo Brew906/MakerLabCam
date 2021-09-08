23cc2a9eb138b7124073d4ae6cc20d5f4b461c6e
Michael Brewer <76227793+Brew906@users.noreply.github.com>
2021-09-08 10:51:37 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,56 @@
+//Michael Brewer
+//6/30/2021
+//Assignment: final project web page
+//This web page displays live video feed to the 3D printers in the maker lab
+//It also has a sign up page for students that are not certified to get files printed
+//This is the java script code for the site
+
+
+function subinfo(){/*this function take the users name and prints it to the web site table*/
+    var nam = document.getElementById("name1").value;
+    document.getElementById("name1re").innerHTML = nam;
+
+}
+function subinfo1(){/*this function take the users email and prints it to the web site table*/
+    var nam = document.getElementById("email1").value;
+    document.getElementById("email1re").innerHTML = nam;
+
+}
+function subinfo2(){/*this function take the date the user inputs and prints it to the web site table*/
+    var nam = document.getElementById("date1").value;
+    document.getElementById("date1re").innerHTML = nam;
+
+}
+
+/******************/
+function subinfo3(){/*this function take the users name and prints it to the web site table*/
+    var nam = document.getElementById("name2").value;
+    document.getElementById("name2re").innerHTML = nam;
+
+}
+function subinfo4(){/*this function take the users email and prints it to the web site table*/
+    var nam = document.getElementById("email2").value;
+    document.getElementById("email2re").innerHTML = nam;
+
+}
+function subinfo5(){/*this function take the date the user inputs and prints it to the web site table*/
+    var nam = document.getElementById("date2").value;
+    document.getElementById("date2re").innerHTML = nam;
+
+}
+/**********************/ 
+function subinfo6(){/*this function take the users name and prints it to the web site table*/
+    var nam = document.getElementById("name3").value;
+    document.getElementById("name3re").innerHTML = nam;
+
+}
+function subinfo7(){/*this function take the users email and prints it to the web site table*/
+    var nam = document.getElementById("email3").value;
+    document.getElementById("email3re").innerHTML = nam;
+
+}
+function subinfo8(){/*this function take the date the user inputs and prints it to the web site table*/
+    var nam = document.getElementById("date3").value;
+    document.getElementById("date3re").innerHTML = nam;
+
+}
